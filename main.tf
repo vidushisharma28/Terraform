629419ff050c7685be2a6079102e4ac57e789acc
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-03 16:10:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,137 @@
+  resource "azurerm_virtual_network" "vnet" {
+    name                = var.vnet
+    address_space       = ["10.0.0.0/16"]
+    resource_group_name = var.resource_group_name
+    location            = var.location
+  }
+
+  resource "azurerm_subnet" "subnet" {
+    name                 = var.subnet
+    resource_group_name = var.resource_group_name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
+  }
+  resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  /*security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }*/
+    security_rule {
+    name                       = "allow-ssh"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  /*security_rule {
+  name                       = "allow-flask"
+  priority                   = 120
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "5000"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+}*/
+  }
+  
+
+  resource "azurerm_public_ip" "mypublicip" {
+      name = var.public_ip_address
+      resource_group_name = var.resource_group_name
+      location            = var.location
+      allocation_method   = "Static"
+      sku = "Standard"
+  }
+
+  resource "azurerm_network_interface" "networkinterface" {
+    name                = var.networkinterface
+    resource_group_name = var.resource_group_name
+    location            = var.location
+
+    ip_configuration {
+      name                          = var.ip_configuration
+      subnet_id                     = azurerm_subnet.subnet.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id = azurerm_public_ip.mypublicip.id
+    }
+  }
+
+  resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.networkinterface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+  resource "azurerm_linux_virtual_machine" "virtualmachine" {
+    name                = var.vm_name
+    resource_group_name = var.resource_group_name
+    location            = var.location
+    size    = "Standard_B2s"
+    admin_username = var.admin_username
+
+    admin_ssh_key {
+      username   = var.admin_username
+      public_key = file(var.public_key)
+    }
+    disable_password_authentication = true
+    network_interface_ids = [
+      azurerm_network_interface.networkinterface.id,
+    ]
+
+    os_disk {
+      caching              = "ReadWrite"
+      storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-jammy"
+      sku       = "22_04-lts"
+      version   = "latest"
+    }
+
+  }
+
+  
+/*
+    connection {
+      type = "ssh"
+      user = var.admin_username
+      private_key = file("~/.ssh/id_rsa")
+      host = azurerm_public_ip.mypublicip.ip_address
+    }
+
+    provisioner "file" {
+      source = "app.py"
+      destination = "/home/${var.admin_username}/app.py"
+    
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "sudo apt update -y",
+        "sudo apt install python3-pip -y",
+        "cd /home/${var.admin_username}/app.py",
+        "sudo pip3 install flask",
+        "nohup python3 ~/var.var.admin_username/app.py"
+       ]
+    }
+  }  */
+
---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "admin_username" {
+  description = "value of adminusername"
+  type = string
+
+}
+
+
+variable "public_key" {
+  type        = string
+  description = "SSH public key for VM login"
+ 
+}
+
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM"
+
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+
+  
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+
+}
+
+variable "vnet" {
+  description = "my vnet"
+  type = string
+
+}
+
+variable "subnet" {
+  description = "my subnet"
+  type = string
+
+}
+
+variable "nsg" {
+  description = "my nsg"
+  type = string
+  
+}
+
+variable "networkinterface" {
+  description = "my NIC"
+  type = string
+
+}
+
+
+variable "public_ip_address" {
+  description = "my publicipaddress"
+  type = string
+
+}
+
+variable "ip_configuration" {
+  description = "my ip configuration"
+  type = string
+
+  
+}
+
+
