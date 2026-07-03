986a31eb08de9d3d0c9ec1b6758f5b1fec9e78e1
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-03 20:09:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@
     name                = var.vm_name
     resource_group_name = var.resource_group_name
     location            = var.location
-    size    = "Standard_B2s"
+    size    = var.size
     admin_username = var.admin_username
 
     admin_ssh_key {
