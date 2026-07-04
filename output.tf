f5544fe79dd4bf5253462942ea857d5c570d0361
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-04 19:45:23 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,5 +0,0 @@
-output "vm_public_ip" {
-  description = "The public IP address assigned to the virtual machine"
-  # 1. Changed "public_ip_address" to the correct Azure attribute "ip_address"
-  value       = azurerm_public_ip.mypublicip.ip_address 
-}
