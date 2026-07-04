3b1d502af845fe130ee75d9b27dadd0366190731
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-04 19:30:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
   description = "The public IP address assigned to the virtual machine"
-  value       = azurerm_public_ip.mypublicip.ip_address
+  value       = azurerm_public_ip.mypublicip.public_ip_address
 }
