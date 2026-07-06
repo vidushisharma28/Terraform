7db3e76fecd5742ddd6481c0d7f00a3dafc686e6
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-06 23:55:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "azurerm_virtual_network" "vnet" {
     source_image_reference {
       publisher = "Canonical"
       offer     = "0001-com-ubuntu-server-jammy"
-      sku       = "22_04-lts"
+      sku       = "22_04-lts-gen2"
       version   = "latest"
     }
 
