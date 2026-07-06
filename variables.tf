41b7e4855d2f22da6cb8503e46c3a9d5f88cf21a
vidushisharma28 <141039650+vidushisharma28@users.noreply.github.com>
2026-07-06 16:16:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,74 +1,73 @@
 variable "admin_username" {
-  description = "value of adminusername"
-  type = string
-
+description = "admin username"
+type = string
+default = "azureuser"  
 }
-
 
 variable "public_key" {
-  type        = string
-  description = "SSH public key for VM login"
- 
+description = "mypublickey"
+type = string
+default = "mnt/workspace/id_rsa.pub"
 }
 
+variable "public_ip_address" {
+description = "mypublicipaddress"
+type = string
+default = "terraformpublicip"
+}
 
 variable "instance" {
-description = "my instance"
-type = map(object({
+  description = "my instance configurations"
+  type = map(object({
     vm_size = string
   }))
+  default = {
+    "vm1" = { vm_size = "Standard_B1s" } # 🌟 Changed to 1 core
+    "vm2" = { vm_size = "Standard_B1s" } # 🌟 Changed to 1 core
+    "vm3" = { vm_size = "Standard_B1s" } # 🌟 Changed to 1 core
+  }
 }
-
+  
 
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
-
-  
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
-
+description = "myrg"
+type = string
+default = "Resourcegroup2"
 }
 
 variable "vnet" {
-  description = "my vnet"
-  type = string
-
+description = "myvnet"
+type = string
+default = "vnet1"
 }
 
 variable "subnet" {
-  description = "my subnet"
-  type = string
-
-}
-
-variable "nsg" {
-  description = "my nsg"
-  type = string
-  
+description = "mysubnet"
+type = string
+default = "subnet1"
 }
 
 variable "networkinterface" {
-  description = "my NIC"
-  type = string
-
+description = "mynetworkinterface"
+type = string
+default = "networkinterface1"
 }
 
-
-variable "public_ip_address" {
-  description = "my publicipaddress"
-  type = string
-
+variable "nsg" {
+description = "mynsg"
+type = string
+default = "nsg1"  
 }
 
 variable "ip_configuration" {
-  description = "my ip configuration"
-  type = string
+description = "myipconfig"
+type = string
+default = "myipconfig1"
 }
 
-
-
+variable "location" {
+description = "location"
+type = string
+default = "EastUS"
+}
 
